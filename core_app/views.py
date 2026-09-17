62e3a82cfe32379688d69036a97c9ca9ea7685e2
M. Hadi Arabnezhad <hadiarabnejad@gmail.com>
2026-09-17 19:24:58 +0330

    Update home view to render 'coming_soon.html' and add HTML template for the coming soon page

--- core_app/views.py
+++ core_app/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("Welcome to the core app!")
+    return render(request, 'core_app/coming_soon.html')

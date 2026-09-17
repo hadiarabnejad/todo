a6924fd1c00a5534f08ade8496a3637fee1526b0
M. Hadi Arabnezhad <hadiarabnejad@gmail.com>
2026-09-17 20:05:06 +0330

    Update admin site titles and enhance 'coming_soon.html' content for improved clarity and engagement

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+admin.site.site_header = 'TaskFlow Admin'
+admin.site.site_title = 'TaskFlow Admin'
+admin.site.index_title = 'Dashboard'
+
 urlpatterns = [
     path('', include('core_app.urls')),
     path('admin/', admin.site.urls),
--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+admin.site.site_header = "TaskFlow Admin"
+admin.site.site_title = "TaskFlow Admin"
+admin.site.index_title = "Dashboard"
+
 # Register your models here.
